454dc4329e369a3f76b3bae1f55c3d7b99cc258f
amkashyap1220 <Alexander.kashyap@gmail.com>
2021-03-10 16:08:13 -0600

    Finished assign 7

---
+++ Assign7/inpost.h
@@ -0,0 +1,19 @@
+/**
+ * @file inpost.h
+ * @author Alexander Kashyap (z1926618@students.niu.edu)
+ * Due: 2021-03-11
+ * 
+ * Assignment 7
+ * Course: CSCI 241
+ * Section: 1
+ */
+
+#ifndef INPOST_H
+#define INPOST_H
+#include <string>
+
+using namespace std;
+
+string convert(const string& infix);
+
+#endif
