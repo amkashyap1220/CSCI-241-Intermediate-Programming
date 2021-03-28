876d5ba548ec708695206f0ee8a162c5862e47ac
amkashyap1220 <Alexander.kashyap@gmail.com>
2021-03-28 11:43:38 -0500

    assig 9 work

--- Assign9/scc.cpp
+++ Assign9/scc.cpp
@@ -1,3 +1,10 @@
+//*****************************************************************************
+//
+//  scc.cpp
+//  CSCI 241 Assignment 9
+//  Created by Alexander Kashyap z1926618
+//
+//*****************************************************************************
 #include "sml.h"
 #include <iostream>
 #include <string>
@@ -7,47 +14,62 @@
 
 using namespace std;
 
-//#define SYMBOL_TABLE_SIZE 1000
-
+/**
+ * 
+ * Symbol table entry.
+ *
+ ********************************************************************/
 struct table_entry
 {
     int symbol;
-    char type;    // 'C' constant, 'L' Simple line number, 'V' variable
-    int location; // Simplesim address (00 to MEMSIZE-1)
+    char type;        // 'C' constant, 'L' Simple line number, 'V' variable
+    int location;     // Simplesim address (00 to MEMSIZE-1)
 };
 
 class scc
 {
 public:
-    static const int MEM_SIZE = 100;
+
+    // Data members
+    static const int MEMORY_SIZE = 100;
     static const int SYMBOL_TABLE_SIZE = 1000;
 
+    // Member functions
     scc();
     void first_pass();
     void second_pass();
     void print_program() const;
 
 private:
-    int memory[MEM_SIZE];
-    int data[MEM_SIZE];
+
+    // Data members
+    int memory[MEMORY_SIZE];
+    
+    int data[MEMORY_SIZE];
     int ndata = 0;
 
     table_entry symbol_table[SYMBOL_TABLE_SIZE];
-    int flags[MEM_SIZE];
+    
+    int flags[MEMORY_SIZE];
 
     int next_instruction_addr = 0;
-    int next_const_or_var_addr = MEM_SIZE - 1;
+    int next_const_or_var_addr = MEMORY_SIZE - 1;
     int next_symbol_table_idx = 0;
 
+    // Member functions
     void memory_check() const;
     void data_check() const;
-    void handle_end();
-    void handle_input(istringstream &ss);
+    void stack_space_check(int location) const;
+
     int get_symbol_location(const string &token);
     int search_symbol_table(int, char) const;
+
+    void handle_end();
+    void handle_input(istringstream &ss);
     void handle_data(istringstream &ss);
     void handle_let(istringstream &ss, const string& buffer);
-    void stack_space_check(int location) const;
+    void handle_print(istringstream &ss);
+    void handle_goto(istringstream &ss);
 };
 
 int main(int argc, char *argv[])
@@ -57,12 +79,18 @@ int main(int argc, char *argv[])
     compiler.first_pass();
     compiler.second_pass();
     compiler.print_program();
+
+    exit(0);
 }
 
+/**
+ * Initializes memory and flags array.
+ *
+ ********************************************************************/
 scc::scc()
 {
-    // Initalize memory and flags arrays
-    for (int i = 0; i < MEM_SIZE; i++)
+    // Initalize memory and flags arrays.
+    for (int i = 0; i < MEMORY_SIZE; i++)
     {
         memory[i] = 7777;
         flags[i] = -1;
@@ -80,27 +108,31 @@ void scc::stack_space_check(int location) const
 
 void scc::first_pass()
 {
-    string buffer1, buffer2, line_number, command;
+    string buffer1, buffer2, command;
+
+    int line_number;
 
     while (getline(cin, buffer1))
     {
-        buffer2 = buffer1; // buffer2 used for 'let'
+        // Save copy of buffer for use in handling let instruction.
+        buffer2 = buffer1;
         istringstream ss(buffer1);
 
+        // Read line number.
         ss >> line_number;
 
-        // ... code to add line_number to symbol table, type 'L' ...
-        symbol_table[next_symbol_table_idx].symbol = stoi(line_number);
+        // Add line number to symbol table
+        symbol_table[next_symbol_table_idx].symbol = line_number;
         symbol_table[next_symbol_table_idx].type = 'L';
         symbol_table[next_symbol_table_idx].location = next_instruction_addr;
         next_symbol_table_idx++;
 
-        // Extract the command
+        // Extract the command from this Simple statement.
         ss >> command;
 
+        // Process the command.
         if (command == "input")
         {
-            // ... code to process 'input' command ...
             handle_input(ss);
         }
         else if (command == "data")
@@ -109,12 +141,13 @@ void scc::first_pass()
         }
         else if (command == "let")
         {
-            // ... code to process 'let' command ...
-
+            /*
             handle_let(istringstream &ss, const string &buffer1);
+            */
         }
         else if (command == "if")
         {
+            /*
             // this and let are the only two more than 1 sml lines
 
             // 60 if x > 10 goto 120
@@ -166,25 +199,60 @@ void scc::first_pass()
             else if (relop == "<")
             {
                     // LECTURE 20; THERE SHOULD BE A branch logic for if_goto.docx
-            }
-
-
+            }*/
         }
         else if (command == "goto")
         {
+            handle_goto(ss);
         }
         else if (command == "print")
         {
+            handle_print(ss);
         }
         else if (command == "end")
         {
             // ... code to process 'end' command ...
             handle_end();
         }
-        else if (command == "rem")
+        else // if (command == "rem")
         {
+            // do nothing
         }
     }
+}
+
+void scc::handle_goto(istringstream &ss)
+{
+    string token;
+
+    ss >> token;
+
+    int idx = search_symbol_table(stoi(token), 'T');
+
+    memory_check();
+    if (idx == -1) // after goto line
+    {
+        memory[next_instruction_addr] = BRANCH * 100;
+        flags[next_instruction_addr] = stoi(token);
+    }
+    else // before goto line
+    {
+        memory[next_instruction_addr] = BRANCH * 100 + symbol_table[idx].location;
+    }
+    next_instruction_addr++;
+}
+
+void scc::handle_print(istringstream &ss)
+{
+    string token;
+
+    ss >> token;
+
+    int location = get_symbol_location(token);
+
+    memory_check();
+    memory[next_instruction_addr] = WRITE * 100 + location;
+    next_instruction_addr++;
 }
 
 void scc::handle_let(istringstream &ss, const string& buffer)
@@ -202,7 +270,7 @@ void scc::handle_let(istringstream &ss, const string& buffer)
 
     // call your convert funciton, passing it the infix string, to get the postfix string to process
 
-    istringstream ss(postfix);
+    istringstream sp(postfix);
     int next_stack_idx = 0;
     while (ss >> token)
     {
@@ -220,7 +288,7 @@ void scc::handle_let(istringstream &ss, const string& buffer)
             memory_check();
             memory[next_instruction_addr] = STORE * 100;
             flags[next_instruction_addr] = -3 - next_stack_idx;
-            next_instruction_addr;
+            next_instruction_addr++;
             next_stack_idx++;
         }
         else if (token == "+")
@@ -335,6 +403,7 @@ void scc::handle_let(istringstream &ss, const string& buffer)
     memory_check();
     memory[next_instruction_addr] = STORE * 100 + lvar_location;
 }
+
 void scc::handle_data(istringstream &ss)
 {
     string token;
@@ -345,9 +414,10 @@ void scc::handle_data(istringstream &ss)
     data[ndata] = stoi(token);
     ndata++;
 }
+
 void scc::memory_check() const
 {
-    if (next_instruction_addr >= MEM_SIZE || next_instruction_addr > next_const_or_var_addr)
+    if (next_instruction_addr >= MEMORY_SIZE || next_instruction_addr > next_const_or_var_addr)
     {
         cout << "*** ERROR: ran out of Simplesim memory ***\n";
         exit(1);
@@ -356,7 +426,7 @@ void scc::memory_check() const
 
 void scc::data_check() const
 {
-    if (ndata >= MEM_SIZE)
+    if (ndata >= MEMORY_SIZE)
     {
         cout << "*** ERROR: too many data lines ***\n";
         exit(1);
@@ -460,6 +530,8 @@ void scc::second_pass()
                 // Search symbol table for line number flags[i]
                 // add that line number's lovation from the symbol table the instruciton
                 // memory[i] += ...
+                int idx = search_symbol_table(flags[i], 'L');
+                memory[i] += symbol_table[idx].location;
             }
             else if (flags[i] == -2) // incomplete right op reg
             {
@@ -485,7 +557,7 @@ void scc::print_program() const
 {
     // Print memory array followed by the data array
     cout << setfill('0') << internal << showpos;
-    for (int i = 0; i < MEM_SIZE; i++)
+    for (int i = 0; i < MEMORY_SIZE; i++)
     {
         cout << setw(5) << memory[i] << endl;
     }
