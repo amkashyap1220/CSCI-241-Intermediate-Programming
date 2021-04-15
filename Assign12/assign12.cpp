4dbf1d0620ec4b5118bfaf57245363252a6c81eb
amkashyap1220 <Alexander.kashyap@gmail.com>
2021-04-15 13:48:36 -0500

    init commit for assign 12, all files

---
+++ Assign12/assign12.cpp
@@ -0,0 +1,19 @@
+/******************************************************************************************************
+ * @file assign12.cpp
+ * @author Alexander Kashyap (z1926618@students.niu.edu)
+ * @version 0.1
+ * @date 2021-04-15
+ * Assignment 12
+ * Due 2021-04-22
+ * @copyright Copyright (c) 2021
+ * 
+******************************************************************************************************/
+#include "shape.h"
+#include "circle.h"
+#include "rectangle.h"
+#include "triangle.h"
+
+int main(int argc, char * argv[])
+{
+    
+}
